754b3a7c48ca64b6611cb55cc578ada6de06e3a4
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-07-21 06:59:18 -0700

    Update basic-javascript.js

--- basic-javascript.js
+++ basic-javascript.js
@@ -59,3 +59,10 @@ function lookUpProfile(name, prop) {
   // Only change code above this line
 }
 lookUpProfile("Akira", "likes");
+
+// Generate Random Number in a range, inclusive
+function randomRange(myMin, myMax) {
+  // Only change code below this line
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  // Only change code above this line
+}
