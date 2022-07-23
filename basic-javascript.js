0c081877ff27f175c4b612ed1ba5e34895b37e73
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-07-22 23:31:40 -0700

    Update basic-javascript.js

--- basic-javascript.js
+++ basic-javascript.js
@@ -67,3 +67,14 @@ function randomRange(myMin, myMax) {
   // Only change code above this line
 }
 
+// Use Recursion to Create a Countdown
+function countup(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countup(n - 1);
+    countArray.push(n);
+    return countArray;
+  }
+}
+console.log(countup(5)); // [1,2,3,4,5]
